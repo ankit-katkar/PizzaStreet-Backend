7b8c7099268e4d2adb85fa7636505e32d2b2662e
Ankit Katkar <ankitkatkar8@.com>
2026-06-27 11:37:05 +0530

    connection issue

--- config/db.js
+++ config/db.js
@@ -4,7 +4,12 @@ import dotenv from "dotenv";
 const connectDB = async () => {
   dotenv.config();
   mongoose.connect(process.env.MONGO_URL)
-    .then(() => console.log("Connected to MongoDB"))
+    .then(() => {
+      console.log("Connected to MongoDB");
+      console.log("Database Name:", mongoose.connection.name);
+      console.log("Connected to:", mongoose.connection.name);
+      console.log("Host:", mongoose.connection.host);
+    })
     .catch((err) => console.error("MongoDB connection error:", err));
 };
 
